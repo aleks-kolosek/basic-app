56f9f9f73151ebce3d8b6c526faa3a110f2841b7
Aleksandar <aleksandar.zoric@ext.webedia-group.com>
2023-04-27 15:59:17 +0200

    One minitest

---
+++ test/models/item_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class ItemTest < ActiveSupport::TestCase
+  test "the truth" do
+    assert true
+  end
+
+  #   3.times do |i|
+  #   test "Test the test no #{i} value" do
+  #     assert_equal(2, i)
+  #   end
+  # end
+end
+
